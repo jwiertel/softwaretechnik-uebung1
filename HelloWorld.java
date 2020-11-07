90b58c3f97eca6a049f38e462b2ff194af34f3d7
jwiertel <73883378+jwiertel@users.noreply.github.com>
2020-11-07 15:08:08 +0100

    #change: print out years at UoC

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
  
        public static void main (String[] args)
        {
-             // Ausgabe Hello World!
-             System.out.println("Hello World!");
+             // Ausgabe 3!
+             System.out.println("3");
        }
 }
